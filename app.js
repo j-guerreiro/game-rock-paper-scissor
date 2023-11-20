5d8a804eb8f53dbbc2eaa189eb49e2513448108a
jguerreiro <joel.borghi@gmail.com>
2023-11-20 20:07:40 -0300

    feature/dev/[Refactoring - breaking code into function chunks]

--- app.js
+++ app.js
@@ -5,17 +5,21 @@ let placarHumano = document.getElementById('humano');
 let placarComputador = document.getElementById('computador');
 let resultado = document.getElementById('resultado');
 let tituloResultado = document.getElementById('titulo-resultado');
+let btns = document.querySelectorAll('button');
 
 /**
  * Insere no array o conteúdo de texto 
  * de cada botão do painel do jogador.
  */
-let btns = document.querySelectorAll('button');
-let btnsTextArray = [];
+function pushButtonText(btns) {
+  let btnsTextArray = [];
+  
+  btns.forEach(function (btn) {
+    btnsTextArray.push(btn.textContent);
+  });
 
-btns.forEach(function (btn) {
-  btnsTextArray.push(btn.textContent);
-});
+  return btnsTextArray;
+}
 
 /**
  * Calcula o resultado do embate 
@@ -26,24 +30,46 @@ function calculaPlacar(humano, computador) {
     return 'Empate!';
   }
 
+  // Chamada de função que remove emoji.
+  removerEmoji(humano, computador);
+
+  // Checagem de vencedor.
+  const resultadoChecagem = checaVencedor(humano, computador);
+  return resultadoChecagem;
+}
+
+/**
+ * Remove o emoji da string para mostrar
+ * o resultado corretamente.
+ */
+function removerEmoji(humano, computador) {
   if (humano.includes('Tesoura')) {
-    humano = humano.substring(0,7).trim();
-  } 
-  
+    humano = humano.substring(0, 7).trim();
+    return humano;
+  }
+
   // Filtra textContent para remover o emoji
   if (computador.includes('Tesoura')) {
-    computador = computador.substring(0,7).trim();
+    computador = computador.substring(0, 7).trim();
+    return computador;
   }
- 
+
   if (computador.includes('Papel') || computador.includes('Pedra')) {
-    computador = computador.substring(0,5).trim();
+    computador = computador.substring(0, 5).trim();
+    return computador;
   }
 
   if (humano.includes('Papel') || humano.includes('Pedra')) {
-    humano = humano.substring(0,5).trim();
+    humano = humano.substring(0, 5).trim();
+    return humano;
   }
+}
 
-  // Checagem de placar
+/**
+ * Função que determina o vencedor
+ * pela checagem da string.
+ */
+function checaVencedor(humano, computador) {
   if (humano.includes('Pedra') && computador.includes('Tesoura')) {
     return 'Jogador venceu!';
   }
@@ -63,7 +89,7 @@ function calculaPlacar(humano, computador) {
   if (computador.includes('Tesoura') && humano.includes('Papel')) {
     return 'Computador venceu!';
   }
-  
+
   if (humano.includes('Pedra') && computador.includes('Papel')) {
     return 'Computador venceu!';
   }
@@ -103,21 +129,21 @@ btns.forEach(function (btn) {
       case 'pedra':
         opcaoHumano = btn.textContent;
         placarHumano.textContent = opcaoHumano;
-        placarComputador.textContent = randomizeMyArray(btnsTextArray);
+        placarComputador.textContent = randomizeMyArray(pushButtonText(btns));
         resultado.textContent = calculaPlacar(placarHumano.textContent, placarComputador.textContent);
         break;
 
       case 'papel':
         opcaoHumano = btn.textContent;
         placarHumano.textContent = opcaoHumano;
-        placarComputador.textContent = randomizeMyArray(btnsTextArray);
+        placarComputador.textContent = randomizeMyArray(pushButtonText(btns));
         resultado.textContent = calculaPlacar(placarHumano.textContent, placarComputador.textContent);
         break;
 
       case 'tesoura':
         opcaoHumano = btn.textContent;
         placarHumano.textContent = opcaoHumano;
-        placarComputador.textContent = randomizeMyArray(btnsTextArray);
+        placarComputador.textContent = randomizeMyArray(pushButtonText(btns));
         resultado.textContent = calculaPlacar(placarHumano.textContent, placarComputador.textContent);
         break;
 
@@ -139,5 +165,3 @@ document.addEventListener("click", function (e) {
     tituloResultado.classList.remove('animacao-placar');
   }, 1500);
 });
-
-
